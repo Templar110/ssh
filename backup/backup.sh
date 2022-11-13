67abab63a870dc1dd1c981b9193c239e69df9e36
Templar110 <106723144+Templar110@users.noreply.github.com>
2022-11-13 11:25:31 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -10,7 +10,7 @@ COLBG1="$(cat /etc/yokkovpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/
 ###########- END COLOR CODE -##########
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/Fikripps/permission/main/ipmini > /root/tmp
+    curl -sS https://raw.githubusercontent.com/Templar110/bot/main/anime > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -28,7 +28,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Fikripps/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/Templar110/bot/main/anime | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -45,7 +45,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Fikripps/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Templar110/bot/main/anime | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -73,7 +73,7 @@ IP=$(curl -sS ipv4.icanhazip.com);
 date=$(date +"%Y-%m-%d")
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-NameUser=$(curl -sS https://raw.githubusercontent.com/Fikripps/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+NameUser=$(curl -sS https://raw.githubusercontent.com/Templar110/bot/main/anime | grep $MYIP | awk '{print $2}')
 
 
 clear
@@ -109,7 +109,7 @@ zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
 ##############++++++++++++++++++++++++#############
 LLatest=`date`
 Get_Data () {
-git clone https://github.com/Fikripps/backupuseryokko.git /root/user-backup/ &> /dev/null
+git clone https://github.com/Templar110/backupuseryokko.git /root/user-backup/ &> /dev/null
 }
 
 Mkdir_Data () {
@@ -130,14 +130,14 @@ Save_And_Exit () {
     DATE=$(date +'%d %B %Y')
     cd /root/user-backup
     git config --global user.email "YakkoRasZ74@gmail.com" &> /dev/null
-    git config --global user.name "Fikripps" &> /dev/null
+    git config --global user.name "Templar110" &> /dev/null
     rm -rf .git &> /dev/null
     git init &> /dev/null
     git add . &> /dev/null
     git commit -m backup &> /dev/null
     git branch -M main &> /dev/null
-    git remote add origin https://github.com/Fikripps/backupuseryokko
-    git push -f https://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@github.com/Fikripps/backupuseryokko.git &> /dev/null
+    git remote add origin https://github.com/Templar110/backupuseryokko
+    git push -f https://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@github.com/Templar110/backupuseryokko.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
@@ -152,7 +152,7 @@ sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Processing updating server...... "
 Save_And_Exit
 fi
-link="https://raw.githubusercontent.com/Fikripps/backupuseryokko/main/$NameUser/$NameUser.zip"
+link="https://raw.githubusercontent.com/Templar110/backupuseryokko/main/$NameUser/$NameUser.zip"
 sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Backup done "
 sleep 1
